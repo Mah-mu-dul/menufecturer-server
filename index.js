462b99ff7099a3c91b3f2e4dfd8a50e27610e52a
habituatedcoder <work.mahmudulhasan@gmail.com>
2022-05-27 23:33:39 +0600

    review api done

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion,ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-const ObjectId = require("mongodb").ObjectID;
 
 
 app.use(cors());
@@ -44,6 +43,7 @@ async function run() {
     const serviceCollection = client.db("electro").collection("services");
     const userCollection = client.db("electro").collection("user");
     const orderCollection = client.db("electro").collection("order");
+    const reviewCollection = client.db("electro").collection("review");
     const profileCollection = client.db("electro").collection("profiles");
 
     app.get("/services", async (req, res) => {
@@ -51,6 +51,13 @@ async function run() {
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+    // find all reviewes  
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
     });
     // find all user to manage admin 
     app.get("/users", async (req, res) => {
@@ -67,8 +74,8 @@ async function run() {
       res.send(orders);
       console.log('try to getting all orders');
     });
-// update role to make admin
-       app.put("/users/:id",verifyJWT, async (req, res) => {
+// update role to make admin   add jwt 
+       app.put("/users/:id", async (req, res) => {
          const id = req.params.id;
          const updatedItem = req.body;
          const filter = { _id: ObjectId(id) };
@@ -89,7 +96,7 @@ async function run() {
       //  update order status for admin to manage orders
        app.put("/order/:id", async (req, res) => {
          const id = req.params.id;
-         console.log('hitted to update ordar status', id);
+         console.log('hitted to update ordar status', id, req.body);
          const updatedItem = req.body;
          const filter = { _id: ObjectId(id) };
          const options = { upsert: true };
@@ -99,10 +106,12 @@ async function run() {
            },
          };
          const result = await orderCollection.updateOne(
-           filter,
+           filter, 
            updatedDoc,
            options
          );
+
+
         //  const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, {expiresIn: '24h'});
          res.send({result}); //add Token after the result
        });
@@ -150,6 +159,12 @@ async function run() {
     app.post("/order", async (req, res) => {
       const order = req.body;
       const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+    // to post a review 
+    app.post("/review", async (req, res) => {
+      const fullReview = req.body;
+      const result = await reviewCollection.insertOne(fullReview);
       res.send(result);
     });
 
